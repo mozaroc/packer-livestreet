f56e7440e667d7ec2fe0e3e886cee755e7b97bd0
Ivan Razin <ir@clodo.ru>
2015-05-20 07:57:21 +0000

    fix cleanup

--- scripts/charmclean.sh
+++ scripts/charmclean.sh
@@ -1,16 +1,7 @@
 #!/bin/sh -ex
 
-if [ -d /var/cache/lxc ]; then
-    rm -rf /var/cache/lxc/*
-fi
-
-names=$(lxc-ls --running)
-if [ "x${names}" != "x" ]; then
-    for name in $names; do
-        lxc-attach -n $name -- apt-get clean
-        lxc-attach -n $name -- find /var/lib/apt -type f | xargs rm -f
-    done
-fi
+apt-get clean
+/var/lib/apt -type f | xargs rm -f
 
 rm -rf /root/charms/
 apt-get -y purge cloud-init
